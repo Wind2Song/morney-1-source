07b187e3d1152a50c744b20976532146df470574
MadarainCong <huangcong0307@163.com>
2021-12-25 11:35:45 +0800

    添加底部icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+// import Button from 'components/Button'
+import Nav from './components/Nav';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`
+// flex-grow: 1;表示标签高度尽量高；下一行表示滚动条就只在这个标签里
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Main>
+          <Switch>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+  <div>
+    <h2>404</h2>
+    宁访问的页面不存在
+  </div>  
+  )
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Button = styled.button`
+color red;
+border:none;
+$:hover{
+  background:red;
+}
+`
+
+export default Button;
