f4adf0a0f3e7cf7528ed5fe2ba6032bc3a98f45a
MadarainCong <huangcong0307@163.com>
2022-01-03 17:41:08 +0800

    创建Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,16 @@ import {
 
   Redirect
 } from "react-router-dom";
+import Layout from 'components/Layout';
 
-const Wrapper = styled.div`
-  border: 1px solid red;
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-`
-// flex-grow: 1;表示标签高度尽量高；下一行表示滚动条就只在这个标签里
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
 
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Main>
           <Switch>
           <Route path="/money">
             <Money />
@@ -48,32 +35,42 @@ function App() {
             <NoMatch />
           </Route>
           </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+        
     </Router>
   );
 }
 
 function NoMatch() {
   return (
-  <div>
-    <h2>404</h2>
-    宁访问的页面不存在
-  </div>  
+    <div>
+      <h2>404</h2>
+      宁访问的页面不存在
+    </div>  
   )
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>        
+    </Layout>
+  )
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout>
+        <h2>记账页面</h2>        
+    </Layout>
+  )
 }
 
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout>
+      <h2>统计页面</h2>        
+    </Layout>
+  )
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import Nav from './Nav';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+// flex-grow: 1;表示标签高度尽量高；下一行表示滚动条就只在这个标签里
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Layout = (props:any) => {
+    return (
+        <Wrapper>
+            <Main>
+                {props.children}        
+            </Main>
+            <Nav />
+        </Wrapper>
+    )
+};
+
+export default Layout;
