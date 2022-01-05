b7fa42c3d5061185752635f7ead26fae13945de8
MadarainCong <huangcong0307@163.com>
2022-01-05 20:45:03 +0800

    记账页 数字面板 UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -39,11 +39,61 @@ font-size: 14px;
 `;
 
 const CategorySection  = styled.section`
-
+background: #c4c4c4;
+font-size: 24px;
+  > ul{
+    display: flex;
+    > li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0px;
+      position: relative;
+      &.selected::after{
+          content:'';
+          display: block;
+          bottom: 0;
+          height: 3px;
+          background: #333;
+          position: absolute;
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
+  }
 `;
 
 const NumberPadSection  = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  > .output{
+    background: white;
+    line-height: 72px;
+    font-size: 36px;
+    text-align: right;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+    > button{
+      float: left;      width: 25%;      height: 64px;      border: none;
+      &.ok{ height: 128px; float: right; }
+      &.zero{ width: 50%;}
+      &:nth-child(1){background: #f2f2f2;}
+      &:nth-child(2),
+      &:nth-child(5){background: #e0e0e0;}
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9){background: #d3d3d3;}
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10){background: #c1c1c1;}
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13){background: #b8b8b8;}
+      &:nth-child(12){background: #9a9a9a;}
+      &:nth-child(14){background: #a9a9a9;}
+    }
+  }
 `;
 
 function Money() {
@@ -66,17 +116,17 @@ function Money() {
           </NotesSection>       
           <CategorySection>
             <ul>
-              <li>支出</li>
+              <li className="selected">支出</li>
               <li>收入</li>
             </ul>
           </CategorySection>
           <NumberPadSection>
-            <div>100</div>
-            <div>
+            <div className='output'>100</div>
+            <div className='pad clearfix' >
               <button>1</button><button>2</button><button>3</button><button>删除</button>
               <button>4</button><button>5</button><button>6</button><button>清空</button>
-              <button>7</button><button>8</button><button>9</button><button>OK</button>
-              <button>0</button><button>.</button>
+              <button>7</button><button>8</button><button>9</button><button className='ok'>OK</button>
+              <button className='zero'>0</button><button>.</button>
             </div>
           </NumberPadSection>
       </Layout>
@@ -84,11 +134,3 @@ function Money() {
 };
 
 export default Money;
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>        
-    </Layout>
-  )
-};
