eb3d6b6b666e7e946fc03fad76840a271c186fcf
MadarainCong <huangcong0307@163.com>
2022-01-09 10:00:20 +0800

    重构 NumberPad

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,37 +1,7 @@
-import styled from "styled-components";
 import React, { useState } from 'react';
-const Wrapper  = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output{
-    background: white;
-    line-height: 72px;
-    font-size: 36px;
-    text-align: right;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad{
-    > button{
-      float: left;      width: 25%;      height: 64px;      border: none;
-      &.ok{ height: 128px; float: right; }
-      &.zero{ width: 50%;}
-      &:nth-    child(1){background: #f2f2f2;}
-      &:nth-child(2),
-      &:nth-child(5){background: #e0e0e0;}
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9){background: #d3d3d3;}
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10){background: #c1c1c1;}
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13){background: #b8b8b8;}
-      &:nth-child(12){background: #9a9a9a;}
-      &:nth-child(14){background: #a9a9a9;}
-    }
-  }
-`;
+import { GeneratorOutput } from './NumberSection/Generatoroutput';
+import { Wrapper } from "./NumberSection/Wrapper";
+
 const NumberPadSection:React.FC = ()=>{
   const [output, _setOutput] = useState('0');
   const setOutput = (output:string) =>{
@@ -46,40 +16,8 @@ const NumberPadSection:React.FC = ()=>{
     // 把被点击的button标签打印出来，打印的是e.target
     const text = (e.target as HTMLButtonElement).textContent;
     if(text === null){return}
-    switch(text){
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if(output === '0'){
-          setOutput(text);
-        }else{
-          setOutput(output + text)
-        };
-        break;
-      case '.':
-        if(output.indexOf('.') >= 0){return;}
-        setOutput(output+'.')
-        break;
-      case '删除':
-        if(output.length === 1){
-          setOutput('0');
-        }else{
-          setOutput(output.slice(0,-1));
-        }
-        break;
-      case '清空':
-        setOutput('0');
-        break;
-      case 'OK':
-        console.log('23300')
-        break;
+    if('0123456789.'.split('').concat(['删除', '清空']).indexOf(text)>=0){
+      setOutput(GeneratorOutput(text,output))
     }
   };
   return (
